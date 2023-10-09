2a9053158590624627301c1e1553ced2dfaeac01
AlexDijkie <dijkhuizenalexander@gmail.com>
2023-10-09 23:12:21 +0200

    Nieuwe dingen

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
 <header>
     <div class="logo">
         <a href="index.php">
-            <img src="images/NerdyGadgets_logo.png" alt="Logo"  width="228" height="114">
+            <img src="images/NerdyGadgets_logo 5.png" alt="Logo"  width="250" height="90">
         </a>
     </div>
 
@@ -233,3 +233,4 @@
 </footer>
 </body>
 </html>
+
