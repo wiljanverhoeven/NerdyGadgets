87b3958eb3cdc9a228351eab3392f95cfe248615
wiljanverhoeven <wiljanverhoeven@xs4all.nl>
2023-10-13 10:37:14 +0200

    .
    .

---
+++ dbconnect.php
@@ -0,0 +1,14 @@
+<?php
+
+//defined variabelen voor de database connectie
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "nerdygadgets";
+
+//connectie met de database
+$conn = new mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
+
+
+
+?>
