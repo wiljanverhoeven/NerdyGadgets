78302cb1b23eae015b0ea1c92354792bae431989
wiljanverhoeven <wiljanverhoeven@xs4all.nl>
2023-10-13 11:48:56 +0200

    changed footer

--- pages/product.php
+++ pages/product.php
@@ -127,6 +127,20 @@
 
     </div>
 
+    <div class="betaalmiddelen">
+    <h2>Veilige betalingsmogelijkheden</h2>
+        <p>Bij NerdyGadgets bieden wij diverse betalingsmogelijkheden aan om uw betalingservaring veilig en vertrouwd te maken:</p>
+            <ul id="list">
+            <a>
+                <img src="images/icon-mastercard.png" width="90" height="56" >
+                <img src="images/IDEAL_Logo.png" width="64.4" height="56" >
+                <img src="images/icon-visa.png" width="90" height="56" >
+            
+            </a>
+        </ul>
+    <p>We nemen ook strenge beveiligingsmaatregelen om ervoor te zorgen dat uw betalingen veilig worden verwerkt en om het vertrouwen van onze klanten te vergroten.</p>
+    </div>
+
 </footer>
 </body>
 </html>
