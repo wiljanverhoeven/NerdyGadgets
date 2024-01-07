3852cf5cf813ea8507a0fa2d76d448a7b83a155d
wiljanverhoeven <wiljanverhoeven@xs4all.nl>
2024-01-07 16:21:58 +0100

    changes

--- dbconnect.php
+++ dbconnect.php
@@ -4,7 +4,7 @@
 $dbServername = "localhost";
 $dbUsername = "root";
 $dbPassword = "";
-$dbName = "mydb";
+$dbName = "mydb"; //als je naam van de database is veranderd hier ook de naam veranderen
 
 //connectie met de database
 $conn = new mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
